7de53ed752dc0dfd68e579172bd965b5c60cb59b
workspace <workspace@workspace>
2019-05-07 21:55:20 -0300

    - Updating dependencies;  - Creating cached entity;  - Updating dependencies;
    Signed-off-by: workspace <workspace@workspace>

---
+++ src/main/java/org/coldis/library/persistence/model/AbstractCachedEntity.java
@@ -0,0 +1,47 @@
+package org.coldis.library.persistence.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.MappedSuperclass;
+
+import org.coldis.library.model.ModelView;
+import org.coldis.library.model.TypedObject;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+/**
+ * Cached entity.
+ */
+@MappedSuperclass
+public abstract class AbstractCachedEntity extends AbstractExpirableEntity implements TypedObject {
+
+	/**
+	 * Serial.
+	 */
+	private static final long serialVersionUID = 6767257519950574276L;
+
+	/**
+	 * When data was cached.
+	 */
+	private LocalDateTime cachedAt;
+
+	/**
+	 * Gets the cachedAt.
+	 *
+	 * @return The cachedAt.
+	 */
+	@JsonView({ ModelView.Persistent.class, ModelView.Public.class })
+	public LocalDateTime getCachedAt() {
+		return this.cachedAt;
+	}
+
+	/**
+	 * Sets the cachedAt.
+	 *
+	 * @param cachedAt New cachedAt.
+	 */
+	public void setCachedAt(final LocalDateTime cachedAt) {
+		this.cachedAt = cachedAt;
+	}
+
+}
