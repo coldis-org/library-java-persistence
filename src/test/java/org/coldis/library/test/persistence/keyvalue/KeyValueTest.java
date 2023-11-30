8615b463ee0bdd37ffb69e0b898f0c9ba83ec6e0
rvcoutinho <me@rvcoutinho.com>
2023-11-30 14:59:53 -0300

    - Improving tests;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/test/java/org/coldis/library/test/persistence/keyvalue/KeyValueTest.java
+++ src/test/java/org/coldis/library/test/persistence/keyvalue/KeyValueTest.java
@@ -226,23 +226,30 @@ public class KeyValueTest {
 	 */
 	@Test
 	public void testLockFailFast() throws Exception {
+		
 		// Resets the locks.
 		this.setLocks(null);
+		
 		// Creates three threads.
-		final LockAndHoldThread thread1 = new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST);
-		final LockAndHoldThread thread2 = new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST);
-		final LockAndHoldThread thread3 = new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST);
+		final List<LockAndHoldThread> threads = List.of(
+
+				new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST), new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST),
+				new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST), new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST),
+				new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST), new LockAndHoldThread(LockBehavior.LOCK_FAIL_FAST)
+
+		);
+		
 		// Starts the threads in parallel.
-		thread1.start();
-		thread2.start();
-		thread3.start();
+		threads.parallelStream().forEach(thread -> thread.start());
+
 		// Waits until all locks have been acquired.
-		Assertions.assertTrue(TestHelper.waitUntilValid(() -> thread1.finished && thread2.finished && thread3.finished, (finished -> finished),
-				TestHelper.VERY_LONG_WAIT * 2, TestHelper.REGULAR_WAIT));
-		Assertions.assertTrue(thread1.finished && thread2.finished && thread3.finished);
-		Assertions.assertFalse(StringUtils.isNotEmpty(thread1.error));
-		Assertions.assertTrue(StringUtils.isNotEmpty(thread2.error) && StringUtils.isNotEmpty(thread3.error));
-		Assertions.assertEquals(1, this.getLocks().size());
+		Assertions.assertTrue(TestHelper.waitUntilValid(() -> threads.stream().allMatch(thread -> thread.finished), (finished -> finished),
+				TestHelper.VERY_LONG_WAIT * 4, TestHelper.REGULAR_WAIT));
+		Assertions.assertTrue(threads.stream().allMatch(thread -> thread.finished));
+		Assertions.assertTrue(threads.stream().anyMatch(thread -> StringUtils.isEmpty(thread.error)));
+		Assertions.assertTrue(threads.stream().anyMatch(thread -> StringUtils.isNotEmpty(thread.error)));
+		Assertions.assertTrue(this.getLocks().size() < 6);
+		
 	}
 
 }
